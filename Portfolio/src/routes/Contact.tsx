1a448a55b1e11dc23aafe4d928549debbde4af72
Rachel Sutton <raesak@gmail.com>
2025-01-29 21:02:07 -0800

    fixed contact form

--- Portfolio/src/routes/Contact.tsx
+++ Portfolio/src/routes/Contact.tsx
@@ -8,24 +8,56 @@
 // The form should have a submit button that sends the email
 // The form should display a success message when the email is sent successfully
 
-import React from "react";
 import Container from "../components/Container";
+import React from "react";
 
-const Contact: React.FC = () => (
-  <Container header="Contact">
-    <p>
-      If you possess matters of great importance—or simply wish to bask in the
-      glory of my wisdom—you may send your inquiries to{" "}
-      <a
-        href="mailto:RoyalDecrees@FlorinCastle.gov"
-        className="text-primary-content underline"
-      >
-        RoyalDecrees@FlorinCastle.gov
-      </a>
-      .
-    </p>
-    <p>Do not waste my time.</p>
-  </Container>
-);
-
-export default Contact;
+export default function Contact() {
+  const ref = React.useRef<HTMLFormElement>(null);
+  function submitHandler(e: React.FormEvent) {
+    e.preventDefault();
+    ref.current?.reset();
+    alert("Message sent successfully!");
+  }
+  return (
+    <Container header="Contact">
+      <p>
+        If you possess matters of great importance—or simply wish to bask in the
+        glory of my wisdom—you may fill out this form.
+      </p>
+      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+        <form ref={ref} onSubmit={submitHandler} className="space-y-8">
+          <div>
+            <label className="block mb-2 text-sm font-medium text-primary-content">
+              Your email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="shadow-sm border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+              placeholder="name@florinisgreat.com"
+              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+              required
+            />
+          </div>
+          <div className="sm:col-span-2">
+            <label className="block mb-2 text-sm font-medium text-primary-content">
+              Your message
+            </label>
+            <textarea
+              id="message"
+              rows={6}
+              className="block p-2.5 w-full text-sm text-gray-900 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
+              placeholder="Do not waste my time."
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="py-3 px-5 text-sm font-medium text-center rounded-lg sm:w-fit border focus:ring-4 focus:outline-none focus:ring-primary-300"
+          >
+            Send message
+          </button>
+        </form>
+      </div>
+    </Container>
+  );
+}
